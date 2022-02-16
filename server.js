19b3be4222fbdf4cdad3823ade68fda4f9c1b062
Yahya <yaya.ly@uadb.edu.sn>
2022-02-16 15:43:26 +0000

    update

--- server.js
+++ server.js
@@ -48,8 +48,7 @@ server.use(function (req, res, next) {
 });
 
 
-mongoose
-    .connect(process.env.MONGO_URI,
+mongoose.connect(process.env.MONGO_URI,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
