d1d5a0405507d56bc49f09f8e63b536c9e148720
Yahya <yaya.ly@uadb.edu.sn>
2022-02-18 16:36:05 +0000

    cors deploy

--- server.js
+++ server.js
@@ -1,63 +1,63 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const server = express()
-const User = require('./model/User')
-const Auth = require('./route/Auth')
-const bodyParser = require('body-parser')
-const UserRoute = require('./route/User')
-const ProductRoute = require('./route/Product')
-const OrderRoute = require('./route/Order')
-const CartRoute = require('./route/Cart')
-const DesignYourCartRoute = require('./route/DesignYourCart')
-const Paydunya = require('./route/Paydunya')
- 
+const express = require("express");
+const mongoose = require("mongoose");
+const server = express();
+const User = require("./model/User");
+const Auth = require("./route/Auth");
+const bodyParser = require("body-parser");
+const UserRoute = require("./route/User");
+const ProductRoute = require("./route/Product");
+const OrderRoute = require("./route/Order");
+const CartRoute = require("./route/Cart");
+const DesignYourCartRoute = require("./route/DesignYourCart");
+const Paydunya = require("./route/Paydunya");
+
 const cors = require("cors");
-const stripe = require('stripe')('sk_test_51KEcipKN0gt4zFdRhEixWzcloryZ0QlYHWhIy9syPWJF8sChb0LIpEj7rRo2byN2xFtxC3W2f2zEaHZLbaXgeIio00mzdNskKH');
+const stripe = require("stripe")(
+  "sk_test_51KEcipKN0gt4zFdRhEixWzcloryZ0QlYHWhIy9syPWJF8sChb0LIpEj7rRo2byN2xFtxC3W2f2zEaHZLbaXgeIio00mzdNskKH"
+);
 
 var corsOptions = {
-    origin: "http://localhost:4200"
+  origin: "https://card-ap.herokuapp.com",
 };
 
 server.use(cors(corsOptions));
 
+server.use(bodyParser.urlencoded({ extended: true }));
 
-server.use(bodyParser.urlencoded({ extended: true }))
-
-server.use(bodyParser.json({ extended: true }))
-
+server.use(bodyParser.json({ extended: true }));
 
 //Routes
 
-server.use('/user', UserRoute)
-server.use('/product', ProductRoute)
-server.use('/order', OrderRoute)
-server.use('/cart', CartRoute)
-server.use('/design',DesignYourCartRoute)
-server.use('/paydunya',Paydunya)
-server.use(express.json())
-server.use('/api', Auth)
+server.use("/user", UserRoute);
+server.use("/product", ProductRoute);
+server.use("/order", OrderRoute);
+server.use("/cart", CartRoute);
+server.use("/design", DesignYourCartRoute);
+server.use("/paydunya", Paydunya);
+server.use(express.json());
+server.use("/api", Auth);
 
 server.use(function (req, res, next) {
-    req.socket.on("error", function () {
-
-    });
-    res.socket.on("error", function () {
-
-    });
-    next();
+  req.socket.on("error", function () {});
+  res.socket.on("error", function () {});
+  next();
 });
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process.env.MONGO_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+const PORT = process.env.PORT || 5000;
+const db = mongoose.connection;
 
-const PORT = process.env.PORT || 5000;        
-const db = mongoose.connection
+db.on("error", (err) => {
+  console.error(err);
+});
+db.once("open", () => {
+  console.log("DB started successfully");
+});
 
-db.on("error", (err) => { console.error(err) })
-db.once("open", () => { console.log("DB started successfully") })
-
-server.listen(PORT, () => { console.log("Server started: 5000") })
+server.listen(PORT, () => {
+  console.log("Server started: 5000");
+});
