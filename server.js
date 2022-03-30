f9d5fedc591a14161a8618049b6d1d11a20d39ff
YahyalBarmaki <yaya.ly@uadb.edu.sn>
2022-03-30 18:30:07 +0200

    update cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const stripe = require("stripe")(
 
 
 var corsOptions = {
-    origin: "*",
+    origin: "http://cart-ud.usinedigitale.org/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
