b9d456a286b2fedeb2737034757d6e3f9622e0be
YahyalBarmaki <yaya.ly@uadb.edu.sn>
2022-03-30 18:24:38 +0200

    update cors

--- Config/passport.js
+++ Config/passport.js
@@ -6,7 +6,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `http://localhost:5000/api/auth/google/callback`,
+            callbackURL: `https://card-ap.herokuapp.com/api/auth/google/callback`,
         },
         async(accessToken, refreshToken, profile, done) => {
             console.log(profile)
