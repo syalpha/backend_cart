19d27f4c839006dc0a8d794be0dca425251f78d9
YahyalBarmaki <yaya.ly@uadb.edu.sn>
2022-03-30 17:39:40 +0200

    update cors

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var corsOptions = {
     credentials: true,
     optionsSuccessStatus: 200,
     allowedHeaders: [
-        'Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept', 'Methods'
+        '*'
     ],
 
 };
